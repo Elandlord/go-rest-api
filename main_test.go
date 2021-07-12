91304a255538bbbb2d5a9ecc629425d08e702cfa
Eric Landheer <e.landheer@mentechmedia.nl>
2021-07-12 12:05:39 +0200

    :rainbow: Basic config test

---
+++ main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+
+	"mentechmedia.nl/rest-api/config"
+)
+
+func TestConfigSetup(t *testing.T) {
+	config := config.GetConfig()
+
+	if config.DB.Connection == "" {
+		t.Fatalf("Config DB Connection not properly set.")
+	}
+
+	if config.DB.Host == "" {
+		t.Fatalf("Config DB Host not properly set.")
+	}
+
+	if config.DB.Port == "" {
+		t.Fatalf("Config DB Port not properly set.")
+	}
+
+	if config.DB.Username == "" {
+		t.Fatalf("Config DB Username not properly set.")
+	}
+
+	if config.DB.Password == "" {
+		t.Fatalf("Config DB Password not properly set.")
+	}
+
+	if config.DB.Charset == "" {
+		t.Fatalf("Config DB Charset not properly set.")
+	}
+}
